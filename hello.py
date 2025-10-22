324ddb0ccc0dde16b91fd99d8663a2574f4d4b53
GGchen <4b3g0116@stust.edu.tw>
2025-10-22 15:01:03 +0800

    Rewrite hello.py with system info in Python version

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+import platform
 
-print('Hello world')
+def get_sys_info():
+    print("System:", platform.system())
+    print("Version:", platform.version())
+    print("Machine:", platform.machine())
+
+if __name__ == "__main__":
+    print("Hello, World! (Python version)")
+    get_sys_info()
 
